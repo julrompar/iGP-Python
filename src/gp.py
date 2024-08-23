6b3c944930b88ade726aa9aaac461cd49622437a
Julián Romero Parejo <114395879+julrompar@users.noreply.github.com>
2024-08-24 01:11:31 +0200

    Cambios en el formato de los csv. Añadido el formato qualy y nuevas operaciones para el cálculo de posiciones medias

--- src/gp.py
+++ src/gp.py
@@ -1,10 +1,17 @@
 from collections import defaultdict, namedtuple
 import csv
+""" Pos,Piloto,Equipo,Finalizado,Mejor vuelta,Velocidad Máxima,Pit,Pts. Estas son las propiedades del csv de gps.
+    La propiedad del csv finalizado no la uso, pero tampoco la quito del documento, por si la necesito en un futuro.
+    TODO: Buscarle un uso a la propiedad finalizado (habría que pasarla a segundos, menos la del primer puesto, que puede ser un 
+    time, y a partir de ahí, hacer un timedelta o algo así)"""
+Carrera = namedtuple('Carrera', 'posicion,piloto,equipo,abreviatura,mejor_vuelta,velocidad_maxima,pitstops,puntos' )
 
-Resultado = namedtuple('Resultado', 'posicion,piloto,equipo,mejor_vuelta,velocidad_maxima,pitstops,puntos' )
-Puntuacion = namedtuple('Puntuacion', 'piloto,puntos')
+Qualy = namedtuple('Qualy', 'posicion,piloto,equipo,abreviatura,tiempo,diferencia,neumaticos' )
+"""Pos,Piloto,Equipo,Mejor Tiempo,Diferencia,Neumáticos. Estas son las propiedades del csv de qualy. La de diferencia no la uso, ya le daré uso en un futuro.
+TODO: Buscarle un uso a la propiedad diferencia"""
 
 def lee_gp(fichero):
+    
     res = []
     with open(fichero, encoding='utf-8') as f:
         lector = csv.reader(f)
@@ -13,37 +20,70 @@ def lee_gp(fichero):
             posicion = int(l[0])
             piloto = l[1].strip()
             equipo= l[2].strip()
-            splitsBySemicolon = l[3].split(":")
+            splitsBySemicolon = l[4].split(":")
             mejor_vuelta = float(60 * int(splitsBySemicolon[0]) + float(splitsBySemicolon[1]))
-            velocidad_maxima = float(l[4])
-            pitstops = int(l[5])
-            puntos = int(l[6])
-            tupla = Resultado(posicion,piloto,equipo,mejor_vuelta,velocidad_maxima,pitstops,puntos)
+            velocidad_maxima = float(l[5])
+            pitstops = int(l[6])
+            puntos = int(l[7])
+            switcher = {
+                "Calamardiño RB": "CRB",
+                "Frenando Afondo F1 Team": "FER",
+                "Hispalis F1 Team": "HIS",
+                "Watiki F1 Team": "WAT",
+                "Saca Racing": "SAR",
+                "Azuaga Martin": "AZM",
+                "DaniDesokupa": "DAD"
+            }
+            tupla = Carrera(posicion,piloto,equipo,switcher.get(equipo),mejor_vuelta,velocidad_maxima,pitstops,puntos)
             res.append(tupla)
     return sorted(res, key=lambda x: x.posicion)
 
-def lee_puntos(fichero):
+def lee_qualy(fichero):
     res = []
     with open(fichero, encoding='utf-8') as f:
         lector = csv.reader(f)
         next(lector)
         for l in lector:
+            posicion = int(l[0])
             piloto = l[1].strip()
-            puntos = int(l[3])
-            tupla = Puntuacion(piloto,puntos)
+            equipo= l[2].strip()
+            tiempo = l[3].strip()
+            diferencia = l[4].strip()
+            neumaticos = l[5].strip()
+            switcherAbreviatura = {
+                "Calamardiño RB": "CRB",
+                "Frenando Afondo F1 Team": "FER",
+                "Hispalis F1 Team": "HIS",
+                "Watiki F1 Team": "WAT",
+                "Saca Racing": "SAR",
+                "Azuaga Martin": "AZM",
+                "DaniDesokupa": "DAD"
+            }
+            switcherNeumaticos = {
+                'SS': 'Superblandos',
+                'S': 'Blandos',
+                'M': 'Medios',
+                'H': 'Duros',
+                'I': 'Intermedios',
+                'W': 'Lluvia'
+            }
+            tupla = Qualy(posicion,piloto,equipo,switcherAbreviatura.get(equipo),tiempo,diferencia,switcherNeumaticos.get(neumaticos))
             res.append(tupla)
-    return res
+    return sorted(res, key=lambda x: x.posicion)
 
 def campeonato(puntuaciones):
-    res = {}
+    res = defaultdict(int)
     for p in puntuaciones:
-        if p.piloto in res:
-            res[p.piloto] += p.puntos
-        else:
-            res[p.piloto] = p.puntos
+        res[p.piloto] += p.puntos
 
     return sorted(res.items(), key=lambda x: x[1], reverse=True)[:3]
 
+def get_campeonato_constructores(puntuaciones):
+    res = defaultdict(int)
+    for p in puntuaciones:
+            res[p.equipo] += p.puntos
+    
+    return sorted(res.items(), key=lambda x: x[1], reverse=True)
 
 def get_resultados_gp_equipo(gp, equipo):
     datos = lee_gp('data/gp_' + gp + '.csv')
@@ -52,6 +92,29 @@ def get_resultados_gp_equipo(gp, equipo):
 def get_resultados_gp(gp):
     datos = lee_gp('data/gp_' + gp + '.csv')
     return [(d.piloto, d.equipo, d.posicion) for d in datos]
+
+def get_pos_media_equipo(decisor, equipo):
+
+    if decisor == 'c' or  decisor == 'C' or decisor == 'Carrera' or decisor == 'carrera':
+        datos_gp = lee_gp('data/gps/puntos.csv')
+        pilotos = [p.piloto for p in datos_gp if p.equipo == equipo or p.abreviatura == equipo]
+        res = defaultdict(float)
+        for p in pilotos:
+            posiciones = [d.posicion for d in datos_gp if d.piloto == p]
+            res[p] = sum(posiciones) / len(posiciones)
+        return sorted(res.items(), key=lambda x: x[1])
+    elif decisor == 'q'or decisor == 'Q' or decisor == 'Qualy' or decisor == 'qualy' or decisor == 'Clasificacion' or decisor == 'clasificacion':
+        datos_qualy = lee_qualy('data/qualy/qualys.csv')
+        pilotos = [p.piloto for p in datos_qualy if p.equipo == equipo or p.abreviatura == equipo]
+        res = defaultdict(float)
+        for p in pilotos:
+            posiciones = [d.posicion for d in datos_qualy if d.piloto == p]
+            res[p] = sum(posiciones) / len(posiciones)
+        return sorted(res.items(), key=lambda x: x[1])
+
+    
+
+
 
 
 
--- src/test_gp.py
+++ src/test_gp.py
@@ -1,12 +1,22 @@
 from gp import *
 
 def test_lectura_unico(leidos):
-    print(len(leidos)) 
+    conjunto = set()
+    for l in leidos:
+        conjunto.add((l.equipo, l.abreviatura))
+    
+    print(conjunto) 
 
+def test_qualy(leidos):
+    print (leidos)
 
 def test_campeonato(puntuaciones):
     clasificacion = campeonato(puntuaciones)
     print(clasificacion) 
+
+def test_campeonato_constructores(puntuaciones):
+        clasificacion = get_campeonato_constructores(puntuaciones)
+        print(clasificacion)
 
 def test_resultados_gp_equipo(gp, equipo):
     resultados = get_resultados_gp_equipo(gp, equipo)
@@ -17,13 +27,21 @@ def test_resultados_gp(gp):
     resultados = get_resultados_gp(gp)
     print (resultados)
 
+def test_pos_media_equipo(decisor, equipo):
+    posiciones = get_pos_media_equipo(decisor, equipo)
+    print(posiciones)
+
 def main():
-    RESULTADOS_GP= lee_gp('data/gp_monaco.csv')
-    PUNTUACIONES = lee_puntos('data/puntos.csv')
+    RESULTADOS_GP= lee_gp('data/gps/gp_austria.csv')
+    RESULTADOS_GENERALES = lee_gp('data/gps/puntos.csv')
+    QUALY_GP = lee_qualy('data/qualy/qualy_austria.csv')
     #test_lectura_unico(RESULTADOS_GP) #El resultado esperado es 14
-    test_campeonato(PUNTUACIONES) #Esperamos los 3 primeros pilotos del campeonato
+    #test_qualy(QUALY_GP) #El resultado esperado es una lista de tuplas con los datos de la clasificacion de la qualy
+    #test_campeonato(RESULTADOS_GENERALES) #Esperamos los 3 primeros pilotos del campeonato
+    #test_campeonato_constructores(PUNTUACIONES) #
     #test_resultados_gp_equipo('monaco', 'HIS') #Esperamos los 2 pilotos del Vela
     #test_resultados_gp('monaco') #Esperamos todos los resultados del gp de Monaco. Solo se muestran los pilotos, los equipos y las posiciones
+    test_pos_media_equipo('q', 'WAT') #Esperamos las posiciones medias de los pilotos del equipo WAT en el campeonato
     
 
 if __name__ == '__main__':
